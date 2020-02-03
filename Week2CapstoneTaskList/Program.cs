e4d22a67a95ca4102e0c3642a71a6bc3d150eea2
Michael <Michael@acdmail.com>
2020-02-02 20:21:53 -0500

    * Week2CapstoneTaskList.dll: Added Mark Task Complete and Quit   functions
    * Program.cs:
    * project.assets.json:
    * Week2CapstoneTaskList.csproj.nuget.cache:
    * Week2CapstoneTaskList.csproj.nuget.g.props:
    * Week2CapstoneTaskList.pdb:
    * Week2CapstoneTaskList.dll:
    * Week2CapstoneTaskList.pdb:
    * Week2CapstoneTaskList.csproj.nuget.dgspec.json:
    * Week2CapstoneTaskList.assets.cache:
    * Week2CapstoneTaskList.runtimeconfig.dev.json:
    * Week2CapstoneTaskList.csproj.FileListAbsolute.txt:
    * Week2CapstoneTaskList.csprojAssemblyReference.cache:

--- Week2CapstoneTaskList/Program.cs
+++ Week2CapstoneTaskList/Program.cs
@@ -7,7 +7,7 @@ namespace Week2CapstoneTaskList
     {
         static void Main(string[] args)
         {
-            List<string> menu = new List<string> { "List tasks", "Add task", "Delete task", "Mark task complete", "Quit" };
+            List<string> menu = new List<string> { "List tasks", "Add task","Edit Task", "Delete task", "Mark task complete", "Quit" };
 
             List<MyTask> listOfTasks = new List<MyTask>
             {
@@ -18,23 +18,20 @@ namespace Week2CapstoneTaskList
                 new MyTask("John", "Take back Winterfell", DateTime.Parse("6/5/1324")),
             };
 
-            Console.WriteLine("Select a number in the menu to perform different actions\n");
-
             bool continueLoop = true;
-            while (continueLoop == true)
+            do
             {
                 PrintList(menu);
-                int input = InputIsInt(menu, Console.ReadLine());
+                int input = InputIsInt(menu, GetUserInput("\nSelect an option: "));
                 if (input == 0) { ListTasks(listOfTasks); }
                 else if (input == 1) { AddTask(listOfTasks); }
-                else if (input == 2) { DeleteTask(listOfTasks); }
-                else if (input == 3) { MarkTaskComplete(listOfTasks); }
-                else if (input == 4){ Quit(); }
+                else if (input == 2) { EditTask(listOfTasks); }
+                else if (input == 3) { DeleteTask(listOfTasks); }
+                else if (input == 4) { MarkTaskComplete(listOfTasks); }
+                else if (input == 5) { continueLoop = false; }
 
-                continueLoop = Option1or2("\nWould you like to select another option? (y/n) : ", "y", "n");
             }
-
-
+            while (continueLoop == true);
         }
 
         public static int InputIsInt(List<MyTask> list, string pick)
@@ -69,15 +66,14 @@ namespace Week2CapstoneTaskList
             while (userContinue != option1 && userContinue != option2)
             {
                 Console.Write(message);
-                userContinue = Console.ReadLine().ToLower();
+                userContinue = Console.ReadLine();
 
-                if (userContinue == "n")
+                if (userContinue == option2)
                 {
                     Console.WriteLine("Okay!!");
                     return false;
                 }
             }
-            Console.WriteLine();
             return true;
         }
         public static string GetUserInput(string message)
@@ -86,25 +82,28 @@ namespace Week2CapstoneTaskList
             string input = Console.ReadLine();
             return input;
         }
+
         public static void PrintList(List<string> listToPrint)
         {
+            Console.WriteLine();
             for (int i = 0; i < listToPrint.Count; i++)
             {
-                Console.WriteLine($"{i + 1}: {listToPrint[i]}");
+                Console.WriteLine($"  {i + 1}: {listToPrint[i]}");
             }
         }
         public static void ListTasks(List<MyTask> listToPrint)
         {
+            Console.WriteLine();
             for (int i = 0; i < listToPrint.Count; i++)
             {
-                Console.WriteLine($"Task {i + 1}: {listToPrint[i].TeamMemberName} \tDue Date: {listToPrint[i].DueDate}  \tCompleted: {listToPrint[i].Completed} \tDescription: {listToPrint[i].Description}");
+                Console.WriteLine($"  Task {i + 1}: {listToPrint[i].TeamMemberName} \tDue Date: {listToPrint[i].DueDate.ToShortDateString()}  \tCompleted: {listToPrint[i].Completed} \tDescription: {listToPrint[i].Description}");
             }
             Console.WriteLine();
         }
         public static void AddTask(List<MyTask> listToAddTo)
         {
-            string name = GetUserInput("Enter name: ");
-            string description = GetUserInput("Enter description: ");
+            string name = GetUserInput("\nTeam Member Name: ");
+            string description = GetUserInput("Task Description: ");
 
             DateTime dueDate = DateTime.Parse("1/1/11");
             bool validDate = false;
@@ -112,7 +111,7 @@ namespace Week2CapstoneTaskList
             {
                 try
                 {
-                    dueDate = DateTime.Parse(GetUserInput("Enter due date (mm/dd/yy): "));
+                    dueDate = DateTime.Parse(GetUserInput("Due Date (mm/dd/yy): "));
                     validDate = true;
                 }
                 catch
@@ -121,23 +120,68 @@ namespace Week2CapstoneTaskList
                     validDate = false;
                 }
             }
+            Console.WriteLine();
             listToAddTo.Add(new MyTask ( name, description, dueDate ));
         }
         public static void DeleteTask(List<MyTask> listToDeleteFrom)
         {
             ListTasks(listToDeleteFrom);
             int num = InputIsInt(listToDeleteFrom, GetUserInput("Select a task to delete: "));
-            listToDeleteFrom.RemoveAt(num);
+            Console.WriteLine($"\n  Task {num + 1}: {listToDeleteFrom[num].TeamMemberName} \tDue Date: {listToDeleteFrom[num].DueDate.ToShortDateString()}  \tCompleted: {listToDeleteFrom[num].Completed} \tDescription: {listToDeleteFrom[num].Description}\n");
+            bool areYouSure = Option1or2("Are you sure? Y/N: ", "Y", "N");
+            if (areYouSure == true)
+            {
+                listToDeleteFrom.RemoveAt(num);
+            }
         }
-        public static void MarkTaskComplete(List<MyTask> listToMarkUp)
+        public static void MarkTaskComplete(List<MyTask> listToMarkComplete)
+        {
+            ListTasks(listToMarkComplete);
+            int num = InputIsInt(listToMarkComplete, GetUserInput("\nSelect a task to mark as complete: "));
+            Console.WriteLine($"\n  Task {num + 1}: {listToMarkComplete[num].TeamMemberName} \tDue Date: {listToMarkComplete[num].DueDate.ToShortDateString()}  \tCompleted: {listToMarkComplete[num].Completed} \tDescription: {listToMarkComplete[num].Description}\n");
+            bool areYouSure = Option1or2("Are you sure? Y/N: ", "Y", "N");
+            if (areYouSure == true)
+            {
+                listToMarkComplete[num].Completed = true;
+            }
+        }
+        public static void EditTask(List<MyTask> listToEdit)
         {
 
+            ListTasks(listToEdit);
+            int taskNum = InputIsInt(listToEdit, GetUserInput("\nSelect a task to edit: "));
+            Console.WriteLine($"\n  Task {taskNum + 1}: {listToEdit[taskNum].TeamMemberName} \tDue Date: {listToEdit[taskNum].DueDate.ToShortDateString()}  \tCompleted: {listToEdit[taskNum].Completed} \tDescription: {listToEdit[taskNum].Description}\n");
+
+            int fieldToEdit = InputIsInt(listToEdit, GetUserInput("1: Team Member\n2: Due Date\n3: Description\n\nWhat field would you like to edit?: "));
+
+            if (fieldToEdit == 0)
+            {
+                Console.WriteLine($"Current Team Member Name: {listToEdit[taskNum].TeamMemberName}\n");
+                listToEdit[taskNum].TeamMemberName = GetUserInput("Enter new name: ");
+            }
+            else if (fieldToEdit == 1)
+            {
+                Console.WriteLine($"Current Due Date: {listToEdit[taskNum].DueDate.ToShortDateString()}\n");
+                bool validDate = false;
+                while (validDate == false)
+                {
+                    try
+                    {
+                        listToEdit[taskNum].DueDate = DateTime.Parse(GetUserInput("Enter new due date (mm/dd/yy): "));
+                        validDate = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Invalid date");
+                        validDate = false;
+                    }
+                }
+            }
+            else if (fieldToEdit == 2)
+            {
+                Console.WriteLine($"Current Description: {listToEdit[taskNum].Description}\n");
+                listToEdit[taskNum].Description = GetUserInput("Enter new description: ");
+            }
         }
-        public static void Quit()
-        {
-
-        }
-
-
     }
 }
